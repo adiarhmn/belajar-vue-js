d65dfdfac4f1cdbd250970c6801e00f0ca062eb1
adiarhmn <adiauliarahmn@gmail.com>
2026-03-24 13:12:43 +0800

    Belajar Reaktif Data

--- src/components/ReaktifData.vue
+++ src/components/ReaktifData.vue
@@ -1,19 +1,49 @@
 <script setup lang="ts">
-import { ref } from 'vue';
+import { onUpdated, reactive, ref } from 'vue';
 
-
+// Data Tunggal seperti string boolean number atau object
 const nama = ref('Reaktif Data');
-const input = ref('')
+const input = ref('');
+
+
+const details = reactive({
+  nama: "Adi Aulia Rahman",
+  umur: 22
+})
+
 
 function gantiNama() {
   nama.value = input.value
 }
+
+function tambahUmur() {
+  details.umur++
+}
+
+function logData(data:string | number) {
+  console.log('render ' + data)
+  return data
+}
+
+onUpdated(() => {
+  console.log("onUpdated")
+})
 </script>
 
 <template>
   <h1>{{ nama }}</h1>
-  <input v-model="input" type="text">
-    <button @click="gantiNama()">
-      Ganti nama
-    </button>
+  <input v-model="input" type="text" />
+  <button @click="gantiNama()">
+    Ganti nama
+  </button>
+
+  <div>
+    <p>{{ logData(details.nama) }}</p>
+    <p>{{ logData(details.umur) }}</p>
+  </div>
+
+  <button @click="tambahUmur()">
+    Tambah Umur
+  </button>
+
 </template>
